cf51a180576e14cb724eed7e72d7ab0f999f139f
Jasminvn <160454837+JasVidal@users.noreply.github.com>
2024-07-21 09:44:29 -0500

    agregar funciones segunda parte

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+/*Caja de texto  */
+
+const txtArea = document.getElementsByName('user-input')[0]; 
+
+/*Funciones para cada métrica */
+
+const chars = document.querySelector('[data-testid=character-count]');
+const charsExS = document.querySelector('[data-testid=character-no-spaces-count]');
+const words = document.querySelector('[data-testid=word-count]');
+const numbers = document.querySelector('[data-testid=number-count]');
+const numbersSum = document.querySelector('[data-testid=number-sum]');
+const average = document.querySelector('[data-testid=word-lenght-average]');
+ 
+
+    txtArea.addEventListener('input', (ev) => {
+        const textareaEl = ev.target
+        const text = textareaEl.value // 'Hola mundo'
+        // -> letter
+
+    const lettersCount = analyzer.getCharacterCount(text)
+    chars.innerHTML = "Carácteres: " + lettersCount
+    // chars.innerText = analyzer.getCharacterCount(text)
+
+    const wordsCount = analyzer.getWordCount(text)
+    words.innerHTML = "Palabras: " + wordsCount
+    //words.innerText = analyzer.getWordCount(text)
+
+    // cambiar el nombre de variable
+    const charsExSCount = analyzer.getCharacterCountExcludingSpaces(text)
+    charsExS.innerHTML = "Carácteres sin espacios: " + charsExSCount
+    
+    const averageCount = analyzer.getAverageWordLength(text)
+    average.innerHTML = "Promedio longitud: " + averageCount
+    
+    const numbersCount = analyzer.getNumberCount(text)
+    numbers.innerHTML = "Números: " + numbersCount
+
+    const numbersSumCount = analyzer.getNumberSum(text)
+    numbersSum.innerHTML = "Suma números: " + numbersSumCount
+
+   
+})
+
+//Botón: Limpiar caja de texto//
+
+const btn = document.getElementById('reset-button')
+btn.addEventListener('click', () => {
+    txtArea.value = '';
+});
