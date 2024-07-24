bcef6a239a7bb1b29ba575b072a26e2324c17395
Jasminvn <160454837+JasVidal@users.noreply.github.com>
2024-07-23 23:16:59 -0500

    agregando nuevos cambios generales

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,22 +1,110 @@
-const analyzer = {  
+const analyzer = {
+
+  /*Función para obtener Contador de Palabras */
+
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+
+    /*"hola mundo".split(' ')->["hola","mundo"] cadena de texto(string) -> arreglo | replace/trim -> texto(string) - texto (string) */
+    return text.replace(/[\d.,?!¡¿;:]/g, '').trim().split(' ').length
   },
+
+
+  /*Función para obtener Contador de Charácteres */
+
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+
+    return text.length
   },
+
+
+  /*Función para obtener Contador de Carácteres Exc. Espacios y Signos */
+
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+
+    /*for,if *//*arreglo con signos de punt *//*uso de semantica, identificadores descriptivos */
+
+
+    /*for - para recorrer texto / arreglo / conjunto de elementos*/
+
+    let charsExS = 0;
+    const symbols = "[ .,?!¡¿;:]"
+
+    for (const letter of text) {
+      if (symbols.includes(letter) === false) { charsExS++ }
+    }
+    return charsExS
   },
-  getAverageWordLength: (text) => {    
+
+  /*let charsExS = 0;
+   for (const letter of text) {
+     if (letter !== " " && letter !== ".") {charsExS++}
+   }
+   return charsExS
+   //return text.replace(/[ .,?!¡¿;:]/g, "").length
+ }, */
+
+
+  /*Función para obtener Contador de Longitud de palabra */
+
+  getAverageWordLength: (text) => {
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+
+    /* transformar text a arreglo,recorrerlo, llegar por cada posicion sumar el largo.lenhgt por cada posicion, el largo total lo divido por el arreglo dividido anteriormente */
+
+
+    const wordC = text.split(' ');
+    let sum = 0;
+
+
+    for (let avrg = 0; avrg < wordC.length; avrg++) {
+
+      sum += wordC[avrg].length
+
+    }
+
+    return sum / wordC.length
+    //Con la suma vas a sacar el promedio de la longitud de las palabra
+
   },
+
+
+  /*Función para obtener Contador de Números */
+
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+
+    let numbC = 0;
+
+    for (const char of text) {
+
+      const num = parseInt(char)
+      if (num >= 0 && num <= 1000) {
+        numbC++
+      }
+    }
+    return numbC;
   },
+
+
+  /*Función para obtener Contador de Suma de Números */
+
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
-  },
-};
 
+    let sum = 0;
+
+    for (const char of text) {
+
+      const numbers = parseInt(char)
+      if (numbers >= 0 && numbers <= 1000) {
+        sum = numbers + sum;
+      }
+    }
+    return sum;
+  },
+
+};
 export default analyzer;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import analyzer from './analyzer.js';
 
 /*Caja de texto  */
 
-const txtArea = document.getElementsByName('user-input')[0]; 
+const txtArea = document.getElementsByName('user-input')[0];
 
 /*Funciones para cada métrica */
 
@@ -13,41 +13,46 @@ const charsExS = document.querySelector('[data-testid=character-no-spaces-count]
 const words = document.querySelector('[data-testid=word-count]');
 const numbers = document.querySelector('[data-testid=number-count]');
 const numbersSum = document.querySelector('[data-testid=number-sum]');
-const average = document.querySelector('[data-testid=word-lenght-average]');
- 
+const average = document.querySelector('[data-testid=word-length-average]');
 
-    txtArea.addEventListener('input', (ev) => {
-        const textareaEl = ev.target
-        const text = textareaEl.value // 'Hola mundo'
-        // -> letter
+txtArea.addEventListener('input', (ev) => {
+  const textareaEl = ev.target
+  const text = textareaEl.value // 'Hola mundo'
+  // -> letter
 
-    const lettersCount = analyzer.getCharacterCount(text)
-    chars.innerHTML = "Carácteres: " + lettersCount
-    // chars.innerText = analyzer.getCharacterCount(text)
+  const lettersCount = analyzer.getCharacterCount(text)
+  chars.innerHTML ="Carácteres: " +lettersCount 
+  // chars.innerText = analyzer.getCharacterCount(text)
 
-    const wordsCount = analyzer.getWordCount(text)
-    words.innerHTML = "Palabras: " + wordsCount
-    //words.innerText = analyzer.getWordCount(text)
+  const wordsCount = analyzer.getWordCount(text)
+  words.innerHTML = "Palabras: " + wordsCount
+  //words.innerText = analyzer.getWordCount(text)
 
-    // cambiar el nombre de variable
-    const charsExSCount = analyzer.getCharacterCountExcludingSpaces(text)
-    charsExS.innerHTML = "Carácteres sin espacios: " + charsExSCount
-    
-    const averageCount = analyzer.getAverageWordLength(text)
-    average.innerHTML = "Promedio longitud: " + averageCount
-    
-    const numbersCount = analyzer.getNumberCount(text)
-    numbers.innerHTML = "Números: " + numbersCount
+  // cambiar el nombre de variable
+  const charsExSCount = analyzer.getCharacterCountExcludingSpaces(text)
+  charsExS.innerHTML = "Carácteres sin espacios: " + charsExSCount 
 
-    const numbersSumCount = analyzer.getNumberSum(text)
-    numbersSum.innerHTML = "Suma números: " + numbersSumCount
+  const averageCount = analyzer.getAverageWordLength(text)
+  average.innerHTML = "Longitud promedio: " + averageCount.toString()
 
-   
+  const numbersCount = analyzer.getNumberCount(text)
+  numbers.innerHTML = "Números: " + numbersCount // "0"
+
+  const numbersSumCount = analyzer.getNumberSum(text)
+  numbersSum.innerHTML = "Suma de números: " + numbersSumCount
+
+
 })
 
 //Botón: Limpiar caja de texto//
 
 const btn = document.getElementById('reset-button')
 btn.addEventListener('click', () => {
-    txtArea.value = '';
+  txtArea.value = '';
+  chars.innerHTML = 'Carácteres';
+  charsExS.innerHTML = 'Carácteres sin espacios';
+  words.innerHTML = 'Palabras';
+  numbers.innerHTML = 'Números';
+  numbersSum.innerHTML = 'Suma de números';
+  average.innerHTML = 'Longitud promedio';
 });
